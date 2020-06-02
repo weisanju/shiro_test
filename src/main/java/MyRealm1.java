4a7a9f1e01ac357f8801491650cc481299064dd4
weisanju <1259103745@qq.com>
2020-06-02 22:55:58 +0800

    init

---
+++ src/main/java/MyRealm1.java
@@ -0,0 +1,30 @@
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+public class MyRealm1 implements Realm {
+    @Override
+    public String getName() {
+        return "myrealm1";
+    }
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        //仅支持UsernamePasswordToken类型的Token
+        return token instanceof UsernamePasswordToken;
+    }
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        System.out.println("正在验证1");
+        String username = (String)token.getPrincipal();  //得到用户名
+        String password = new String((char[])token.getCredentials()); //得到密码
+        if(!"zhang".equals(username)) {
+
+            throw new UnknownAccountException("用户名错误"); //如果用户名错误
+        }
+        if(!"123".equals(password)) {
+            System.out.println("密码错误");
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
---
+++ src/main/java/ShiroTest.java
@@ -0,0 +1,27 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShiroTest {
+
+    @Test
+    public void test() {
+//1. Load the INI configuration
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager manager = factory.getInstance();
+        SecurityUtils.setSecurityManager(manager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhangsan", "lisi");
+        subject.login(token);
+        System.out.println(subject.isAuthenticated());
+
+        System.out.println(subject.hasRole("admin"));
+
+        subject.checkPermission("mainpage");
+    }
+}
